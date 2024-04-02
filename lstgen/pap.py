4367f019e43bb03bbb7353f5e8702bfe3ceb82ad
Antonio Masotti <masotti@bikeleasing.de>
2024-04-02 10:12:43 +0200

    feat:  add PAP resources for 2024

--- lstgen/pap.py
+++ lstgen/pap.py
@@ -98,6 +98,10 @@ PAP_RESOURCES = OrderedDict((
         '/interface/2023AbJuliVersion1.xhtml',
         '/javax.faces.resource/daten/xmls/Lohnsteuer2023AbJuli.xml.xhtml'
     )),
+    ('2024_1', PapResource(
+        '/interface/2024Version1.xhtml',
+        '/javax.faces.resource/daten/xmls/Lohnsteuer2024.xml.xhtml'
+    )),
 ))
 
 
